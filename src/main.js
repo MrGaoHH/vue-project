45a25ad1464f94d51f9ea51da8b9568308708b7b
李广涛 <45238864@qq.com>
2018-10-20 22:13:44 +0800

    完成首页轮播图和九宫格内容

--- src/main.js
+++ src/main.js
@@ -1,14 +1,31 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router/index.js'  
 
-import './assets/mui/css/mui.min.css'
 
+// import { Swipe, SwipeItem } from 'mint-ui';
+
+// Vue.component(Swipe.name, Swipe);
+// Vue.component(SwipeItem.name, SwipeItem);
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
+
+//
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+Vue.use(MintUI)
+//
+import '@/assets/mui/css/mui.min.css'
+//
+import '@/assets/mui/css/icons-extra.css'
 Vue.config.productionTip = false
+//导入router.js 路由模块\
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import HomeContainer from '@/components/tabbar/HomeContainer.vue'
+import MemberContainer from '@/components/tabbar/MemberContainer.vue'
+import ShopcarContainer from '@/components/tabbar/ShopcarContainer.vue'
+import SearchContainer from '@/components/tabbar/SearchContainer.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path:'/',
+      redirect:'/home'
+    },
+    {
+      path: '/home',
+      component: HomeContainer
+    },
+    {
+      path: '/member',
+      component: MemberContainer
+    },
+    {
+      path: '/shopcar',
+      component: ShopcarContainer
+    },
+    {
+      path: '/search',
+      component: SearchContainer
+    },
+    {
+      path:'*',
+      redirect:'/'
     }
-  ]
+  ],
+  linkActiveClass: 'mui-active' //覆盖默认的路由高亮类
 })
+// //暴露路由对象
+// export default router
